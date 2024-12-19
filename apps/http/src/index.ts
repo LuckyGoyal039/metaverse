fab9b6318bfe7c600009d3bd95f355a05221b901
Lucky Goyal <goyallucky2020@gmail.com>
2024-12-19 21:36:03 +0530

    remove app.use *

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -7,11 +7,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", router)
-app.use('*', (req,res)=>{
-    
-    console.log("call", req.path)
-    res.send("hi from backend")
-})
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`http server running on port ${PORT}`)
